d21c5b848df8281a096aa782e86177d82855fd28
Dominik Joppa <domjoppa@gmail.com>
2022-07-09 19:13:46 +0200

    update fix form

--- src/api/form/index.js
+++ src/api/form/index.js
@@ -4,116 +4,116 @@ const supabase = require('../../config/supabase');
 const router = express.Router();
 
 router.post('/models', async (req, res) => {
-  const {
-    social, username, email
-  } = req.body;
+  const { social, username, email } = req.body;
   // insert into social table
   if (!social || !username || !email) {
     return res.status(400).json({
-      message: 'Please provide all required fields'
+      message: 'Please provide all required fields',
     });
   }
 
   try {
-    const { data: social_data } = await supabase.insert('creators_form', { type: social, creators_id: username, email });
+    const { data: social_data } = await supabase
+      .form('creators_form')
+      .insert({ type: social, creators_id: username, email });
 
     const dataJSON = {
       status: 'success',
       message: `${social} - Your suggestion has been added and is awaiting verification.`,
-      data: social_data
+      data: social_data,
     };
     return res.status(201).json(dataJSON);
   } catch (error) {
     const errorJSON = {
       status: 'error',
-      message: error.message
+      message: error.message,
     };
     res.status(500).json(errorJSON);
   }
 });
 
 router.post('/addProfile', async (req, res) => {
-  const {
-    username, email
-  } = req.body;
+  const { username, email } = req.body;
   // insert into social table
   const social = 'addProfile';
   if (!username || !email) {
     return res.status(400).json({
-      message: 'Please provide all required fields'
+      message: 'Please provide all required fields',
     });
   }
 
   try {
-    const { data: social_data } = await supabase.insert('creators_form', { type: social, creators_id: username, email });
+    const { data: social_data } = await supabase
+      .form('creators_form')
+      .insert({ type: social, creators_id: username, email });
 
     const dataJSON = {
       status: 'success',
       message: 'Your suggestion has been added and is awaiting verification.',
-      data: social_data
+      data: social_data,
     };
     return res.status(201).json(dataJSON);
   } catch (error) {
     const errorJSON = {
       status: 'error',
-      message: error.message
+      message: error.message,
     };
     res.status(500).json(errorJSON);
   }
 });
 
 router.post('/addTrialLink', async (req, res) => {
-  const {
-    username, email, social
-  } = req.body;
+  const { username, email, social } = req.body;
   // insert into social table
   if (!username || !email) {
     return res.status(400).json({
-      message: 'Please provide all required fields'
+      message: 'Please provide all required fields',
     });
   }
 
   try {
-    const { data: social_data } = await supabase.insert('creators_form', { type: social, creators_id: username, email });
+    const { data: social_data } = await supabase
+      .from('creators_form')
+      .insert({ type: social, creators_id: username, email });
 
     const dataJSON = {
       status: 'success',
       message: 'Your suggestion has been added and is awaiting verification.',
-      data: social_data
+      data: social_data,
     };
     return res.status(201).json(dataJSON);
   } catch (error) {
     const errorJSON = {
       status: 'error',
-      message: error.message
+      message: error.message,
     };
     res.status(500).json(errorJSON);
   }
 });
 
 router.post('/category', async (req, res) => {
-  const {
-    category, username, email
-  } = req.body;
+  const { category, username, email } = req.body;
   // insert into category table
   if (!category || !username || !email) {
     return res.status(400).json({
-      message: 'Please provide all required fields'
+      message: 'Please provide all required fields',
     });
   }
 
   try {
-    const { data: categoryData } = await supabase.insert('creators_form', { type: category, creators_id: username, email });
+    const { data: categoryData } = await supabase
+      .form('creators_form')
+      .insert({ type: category, creators_id: username, email });
     const dataJSON = {
       status: 'success',
       message: 'Your suggestion has been added and is awaiting verification.',
-      data: categoryData
+      data: categoryData,
     };
     return res.status(201).json(dataJSON);
   } catch (error) {
     const errorJSON = {
       status: 'error',
-      message: error.message
+      message: error.message,
     };
     res.status(500).json(errorJSON);
   }
